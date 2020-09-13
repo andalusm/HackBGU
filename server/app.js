08f2a92569b9f94165d0195414989c39a57be592
andalusm <37974151+andalusm@users.noreply.github.com>
2020-09-13 18:45:15 +0300

    mongoose code and app

--- server/app.js
+++ server/app.js
@@ -1,69 +1,43 @@
 //jshint esversion:6
-
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect("mongodb://localhost:27017/studentDB",{ useUnifiedTopology: true ,useNewUrlParser: true});
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const studentSchema = new mongoose.Schema({
+   name: String,
+   score: Number
+});
+const Student = mongoose.model('Student', studentSchema);
 const app = express();
-const postSchema = {
-  title: String,
-  content: String
-};
-const Post = mongoose.model('Post', postSchema);
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-let posts = [];
 
-app.get("/", function(req, res){
-  Post.find({},function(err,result){
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: result
-      });
-  });
-
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/ranking", function(req, res) {
+  Student.find({},function(err,students){
+students.sort(function(a,b){
+  return b.score-a.score;
+})
+    res.render("ranking",{students:students,name:"Baraa"});
+});
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
 
-app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  const write = new Post(post);
-  write.save(function(err){
-    if(!err)
-    {
-      res.redirect("/");
-    }
-  });
-
-});
-
-app.get("/posts/:postID", function(req, res){
+app.get("/video/:postID", function(req, res){
   const postID = req.params.postID;
   Post.findById(postID, function (err, post) {
     if(err || !post)
@@ -80,6 +54,8 @@ app.get("/posts/:postID", function(req, res){
 
 });
 
+
+
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started successfully");
 });
